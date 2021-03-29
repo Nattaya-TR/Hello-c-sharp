653e8327c77a78b38c9a063d02030ff7ad81719a
Nattaya Trouillard <nattaya.trouille@gmail.com>
2021-03-29 16:38:20 +0200

    add if statement

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Hello_c_sharp
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Hello Who are you?");
+            string User = Console.ReadLine();
+            Console.WriteLine("Hello " + User + " How are you ?");
+            String Mood = Console.ReadLine();
+            if (Mood == "good")
+            {
+                Console.WriteLine("I feel " + Mood + " as well " + User);
+            }
+            else
+            {
+                Console.WriteLine("Ohhh sorry to hear that, " + User);
+            }
+        
+        }
+    }
+}
